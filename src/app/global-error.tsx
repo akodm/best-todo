5daac3cb9e081b848033e566bb88721730190d13
akodm <a8456452@naver.com>
2024-08-02 00:50:38 +0900

    storybook add

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 interface ErrorType {
   error: Error & { digest?: string };
